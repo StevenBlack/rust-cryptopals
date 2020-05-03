519579043c960f465ef52c39d5a53b8622fcaf09
Steven Black <steveb@stevenblack.com>
2020-05-03 01:41:12 -0400

    Issue #3: fix — cycle() the key to eliminate the key length constraint, and apply basic rules to test the resulting plaintext.

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@
 
 use hex;
 use base64;
+use std::str;
+// extern crate regex;
+use regex::Regex;
 
 fn main() {
   wrap(&challenge1);
   wrap(&challenge2);
+  wrap(&challenge3);
 }
 
 fn hex_to_base64(h: &str) -> String {
@@ -16,10 +20,57 @@ fn hex_to_base64(h: &str) -> String {
 fn hex_xor(h: &str, k: &str) -> String {
   let o: Vec<u8> = hex::decode(h).unwrap()
     .iter()
-    .zip(hex::decode(k).unwrap().iter())
+    .zip(hex::decode(k).unwrap().iter().cycle())
     .map(|(&x1, &x2)| x1 ^ x2)
     .collect();
   return hex::encode(o);
+}
+
+fn challenge3() {
+  println!("Challenge 3");
+  // The hex encoded string:
+  // 1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736
+  // ... has been XOR'd against a single character. Find the key, decrypt the message.
+
+  let hex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+  println!("The hex is: {}", hex);
+  println!("Possible plaintext:");
+
+
+  // presume alphanumeric
+  let re = Regex::new(r"[[:alpha:]]").unwrap();
+  // presume at least one space
+  let rs = Regex::new(r" ").unwrap();
+  // presume vowel
+  let rvo = Regex::new(r"[a,e,i,o,u,A,E,I,O,U]").unwrap();
+
+  // presume no line feeds, carriage returns, or vertical tabs
+  let rl = Regex::new(r"\n").unwrap();
+  let rr = Regex::new(r"\r").unwrap();
+  let rv = Regex::new(r"\v").unwrap();
+
+  let mut count = 0;
+
+  for kn in 1..=255 {
+    let key = hex::encode([kn]).to_string();
+    // hex::decode returns a rust Result<&str, Utf8Error>
+    let res = hex::decode(hex_xor(hex, &key));
+    let s = match res {
+      Ok(v) => v,
+      Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+
+    // check s for vowels
+    // println!("{:?}", s);
+
+    let result = String::from_utf8_lossy(&s);
+    // checks
+    if re.is_match(&result) && rvo.is_match(&result) && rs.is_match(&result) && !rl.is_match(&result) && !rr.is_match(&result) && !rv.is_match(&result) {
+      count = count + 1;
+      println!("key is: {}, output is: {}", &key, result);
+    }
+  }
+  println!("Candidates found: {}", count);
 }
 
 fn challenge2() {
