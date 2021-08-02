36fbde84238b15e4f38b43f523a2e77da2bddfd9
Steven Black <steveb@stevenblack.com>
2021-08-02 01:04:30 -0400

    Issue #6: fix — lets keep track of results so we can pick the top-n candidates for further analysis.

--- src/main.rs
+++ src/main.rs
@@ -6,288 +6,331 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 mod crpt {
-  use base64;
-  use regex::Regex;
+    use base64;
+    use regex::Regex;
 
-  pub struct KV {
-    pub key: u8,
-    pub value: String,
-  }
+    pub struct KV {
+        pub key: u8,
+        pub value: String,
+    }
 
-  pub fn plausible_text(hex: &str) -> Vec<KV> {
-    let mut ret_vec = Vec::new();
-    // presume alphanumeric
-    let ralpha = Regex::new(r"[[:alpha:]]").unwrap();
-    // presume at least one space
-    let rs = Regex::new(r" ").unwrap();
-    // presume vowel
-    let rvowel = Regex::new(r"[a,e,i,o,u,A,E,I,O,U]").unwrap();
-    // presume no line feeds, carriage returns, or vertical tabs
-    // let rl = Regex::new(r"\n").unwrap();
-    let rr = Regex::new(r"\r").unwrap();
-    let rv = Regex::new(r"\v").unwrap();
+    pub fn plausible_text(hex: &str) -> Vec<KV> {
+        let mut ret_vec = Vec::new();
+        // presume alphanumeric
+        let ralpha = Regex::new(r"[[:alpha:]]").unwrap();
+        // presume at least one space
+        let rs = Regex::new(r" ").unwrap();
+        // presume vowel
+        let rvowel = Regex::new(r"[a,e,i,o,u,A,E,I,O,U]").unwrap();
+        // presume no line feeds, carriage returns, or vertical tabs
+        // let rl = Regex::new(r"\n").unwrap();
+        let rr = Regex::new(r"\r").unwrap();
+        let rv = Regex::new(r"\v").unwrap();
 
-    for kn in 0..=255 {
-      let key = hex::encode([kn]).to_string();
-      // hex::decode returns a rust Result<&str, Utf8Error>
-      let res = hex::decode(hex_xor(hex, &key));
-      let s = match res {
-        Ok(v) => v,
-        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-      };
+        for kn in 0..=255 {
+            let key = hex::encode([kn]).to_string();
+            // hex::decode returns a rust Result<&str, Utf8Error>
+            let res = hex::decode(hex_xor(hex, &key));
+            let s = match res {
+                Ok(v) => v,
+                Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+            };
 
-      // discard if any u8 element is above 128
-      if s.iter().max().unwrap() > &127u8 {
-        break;
-      }
+            // discard if any u8 element is above 128
+            if s.iter().max().unwrap() > &127u8 {
+                break;
+            }
 
-      let result = String::from_utf8_lossy(&s);
-      // checks
-      if ralpha.is_match(&result)
+            let result = String::from_utf8_lossy(&s);
+            // checks
+            if ralpha.is_match(&result)
         && rvowel.is_match(&result)
         && rs.is_match(&result)
         // && !rl.is_match(&result)
         && !rr.is_match(&result)
         && !rv.is_match(&result)
-      {
-        let foo = KV {
-          key: kn,
-          value: result.to_string(),
-        };
-        ret_vec.push(foo);
-      }
+            {
+                let foo = KV {
+                    key: kn,
+                    value: result.to_string(),
+                };
+                ret_vec.push(foo);
+            }
+        }
+        return ret_vec;
     }
-    return ret_vec;
-  }
 
-  pub fn encrypt_to_hex(key: String, message: String) -> String {
-    let vecu8: Vec<u8> = message
-      .into_bytes()
-      .iter()
-      .zip(key.into_bytes().iter().cycle())
-      .map(|(&x1, &x2)| x1 ^ x2)
-      .collect();
-    return hex::encode(vecu8);
-  }
+    pub fn encrypt_to_hex(key: String, message: String) -> String {
+        let vecu8: Vec<u8> = message
+            .into_bytes()
+            .iter()
+            .zip(key.into_bytes().iter().cycle())
+            .map(|(&x1, &x2)| x1 ^ x2)
+            .collect();
+        return hex::encode(vecu8);
+    }
 
-  // hex_to_base64 takes a hex str and returns a base64 String.
-  pub fn hex_to_base64(h: &str) -> String {
-    return base64::encode(hex::decode(h).unwrap());
-  }
+    // hex_to_base64 takes a hex str and returns a base64 String.
+    pub fn hex_to_base64(h: &str) -> String {
+        return base64::encode(hex::decode(h).unwrap());
+    }
 
-  // hex_xor takes a hex str and a hey str and returns the xor as a string
-  pub fn hex_xor(h: &str, k: &str) -> String {
-    let o: Vec<u8> = hex::decode(h)
-      .unwrap()
-      .iter()
-      .zip(hex::decode(k).unwrap().iter().cycle())
-      .map(|(&x1, &x2)| x1 ^ x2)
-      .collect();
-    return hex::encode(o);
-  }
+    // hex_xor takes a hex str and a hey str and returns the xor as a string
+    pub fn hex_xor(h: &str, k: &str) -> String {
+        let o: Vec<u8> = hex::decode(h)
+            .unwrap()
+            .iter()
+            .zip(hex::decode(k).unwrap().iter().cycle())
+            .map(|(&x1, &x2)| x1 ^ x2)
+            .collect();
+        return hex::encode(o);
+    }
 }
 
 fn main() {
-  wrap(&challenge1, 0);
-  wrap(&challenge2, 0);
-  wrap(&challenge3, 0);
-  wrap(&challenge4, 0);
-  wrap(&challenge5, 0);
-  wrap(&challenge6prelim, 0);
-  wrap(&challenge6, 1);
+    wrap(&challenge1, 0);
+    wrap(&challenge2, 0);
+    wrap(&challenge3, 0);
+    wrap(&challenge4, 0);
+    wrap(&challenge5, 0);
+    wrap(&challenge6prelim, 0);
+    wrap(&challenge6, 1);
 }
 
 fn challenge6() {
-  println!("Challenge 6");
-  use hamming::distance;
-  use itertools::Itertools;
-  #[derive(Debug, Default)]
-  struct Cumulator {
-    entries: Vec<u64>,
-  }
+    println!("Challenge 6");
+    use hamming::distance;
+    use itertools::Itertools;
+    use std::fmt;
 
-  impl Cumulator {
-    pub fn sum(self: &Self) -> u64 {
-      self.entries.iter().sum()
+    #[derive(Debug, Default)]
+    struct Cumulator {
+        entries: Vec<u64>,
     }
-    pub fn len(self: &Self) -> u64 {
-      return self.entries.len() as u64;
-    }
-    pub fn avg(self: &Self) -> f32 {
-      if self.entries.len() == 0 {
-        return 0.0;
-      }
-      return self.sum() as f32 / self.entries.len() as f32;
-    }
-    pub fn push(&mut self, n: u64) {
-      self.entries.push(n);
-    }
-  }
 
-  // step 1: load the file into a string
-  // read the file line by line and consolidate
-  let mut ciphertext: String = "".to_string();
-  if let Ok(lines) = read_lines("./files/challenge6.txt") {
-    // Consumes the iterator, returns an (Optional) String
-    for l in lines {
-      if let Ok(line) = l {
-        ciphertext.push_str(&line)
-      }
+    impl Cumulator {
+        pub fn sum(self: &Self) -> u64 {
+            self.entries.iter().sum()
+        }
+        pub fn len(self: &Self) -> u64 {
+            return self.entries.len() as u64;
+        }
+        pub fn avg(self: &Self) -> f32 {
+            if self.entries.len() == 0 {
+                return 0.0;
+            }
+            return self.sum() as f32 / self.entries.len() as f32;
+        }
+        pub fn push(&mut self, n: u64) {
+            self.entries.push(n);
+        }
     }
-  }
-  // step 2: Take two consecutive KeyLength chunks, calculate the distance between them
-  let cipherbytes = base64::decode(ciphertext).unwrap();
 
-  // kl is key length
-  for kl in 1..=40 {
-    let mut cumulator = Cumulator::default();
-    // our sampling in combinations of 2
-    for it in (0..2).combinations(2) {
-      let klvec = cipherbytes.chunks(kl).collect_vec();
-      let dist: u64 = distance(klvec[it[0]], klvec[it[1]]);
-      cumulator.push(dist);
+    // step 1: load the file into a string
+    // read the file line by line and consolidate
+    let mut ciphertext: String = "".to_string();
+    if let Ok(lines) = read_lines("./files/challenge6.txt") {
+        // Consumes the iterator, returns an (Optional) String
+        for l in lines {
+            if let Ok(line) = l {
+                ciphertext.push_str(&line)
+            }
+        }
     }
-    println!(
-      "Key length: {} - length: {} - sum: {} - average: {:.4}",
-      kl,
-      cumulator.len(),
-      cumulator.sum(),
-      cumulator.avg() / kl as f32
-    );
-  }
+    // step 2: Take two consecutive KeyLength chunks, calculate the distance between them
+    let cipherbytes = base64::decode(ciphertext).unwrap();
+
+    // kl is key length
+    let klmin: usize = 2;
+    let klmax: usize = 40;
+    // the number of blocks to pair and test
+    let testblocks: usize = 3;
+
+    #[derive(Debug)]
+    pub struct Resultat {
+        pub length: u32,
+        pub avgdistance: f32,
+    }
+
+    impl fmt::Display for Resultat {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            // Use `self.number` to refer to each positional data point.
+            write!(f, "Length: {}\tAvg: {}\n", self.length, self.avgdistance)
+        }
+    }
+
+    struct Foo(Vec<Resultat>);
+
+    impl fmt::Display for Foo {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "Values:\n")?;
+            for v in &self.0 {
+                write!(f, "\t{}", v)?;
+            }
+            Ok(())
+        }
+    }
+
+    let mut resultats = Vec::new();
+
+    for kl in klmin..=klmax {
+        let mut cumulator = Cumulator::default();
+        // our sampling in combinations of 2
+        for it in (0..testblocks).combinations(2) {
+            let klvec = cipherbytes.chunks(kl).collect_vec();
+            let dist: u64 = distance(klvec[it[0]], klvec[it[1]]);
+            cumulator.push(dist);
+        }
+
+        resultats.push(Resultat {
+            // length: cumulator.len() as u32,
+            length: kl as u32,
+            avgdistance: cumulator.avg() / kl as f32,
+        });
+
+        println!(
+            "Key length: {} - length: {} - sum: {} - average: {:.2}",
+            kl,
+            cumulator.len(),
+            cumulator.sum(),
+            cumulator.avg() / kl as f32
+        );
+    }
+    resultats.sort_by(|a, b| a.avgdistance.partial_cmp(&b.avgdistance).unwrap());
+    println!("{:?}", &resultats[0..4]);
 }
 
 fn challenge6prelim() {
-  use hamming::distance;
-  println!("Challenge 6 preliminary");
-  let s1 = b"this is a test";
-  let s2 = b"wokka wokka!!!";
-  assert_eq!(distance(s1, s2), 37);
+    use hamming::distance;
+    println!("Challenge 6 preliminary");
+    let s1 = b"this is a test";
+    let s2 = b"wokka wokka!!!";
+    assert_eq!(distance(s1, s2), 37);
 }
 
 fn challenge5() {
-  println!("Challenge 5");
-  // feed it the string:
-  // Burning 'em, if you ain't quick and nimble
-  // I go crazy when I hear a cymbal
-  // ... after XOR'd with `iCE`
-  // ... should produce:
-  // 0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272
-  // a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f
+    println!("Challenge 5");
+    // feed it the string:
+    // Burning 'em, if you ain't quick and nimble
+    // I go crazy when I hear a cymbal
+    // ... after XOR'd with `iCE`
+    // ... should produce:
+    // 0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272
+    // a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f
 
-  let msg = indoc!(
-    r#"
+    let msg = indoc!(
+        r#"
   Burning 'em, if you ain't quick and nimble
   I go crazy when I hear a cymbal"#
-  )
-  .to_string();
-  let key = "ICE".to_string();
-  let expected = indoc!(
+    )
+    .to_string();
+    let key = "ICE".to_string();
+    let expected = indoc!(
     r#"
   0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"#
   )
   .to_string();
-  let result = crpt::encrypt_to_hex(key.clone(), msg.clone());
-  assert_eq!(expected, result);
+    let result = crpt::encrypt_to_hex(key.clone(), msg.clone());
+    assert_eq!(expected, result);
 
-  println!("The hex is: {}", msg.clone());
-  println!("The key is: {}", key.clone());
-  println!("The XOR output is:   {}", result);
-  println!("The XOR expected is: {}", expected);
+    println!("The hex is: {}", msg.clone());
+    println!("The key is: {}", key.clone());
+    println!("The XOR output is:   {}", result);
+    println!("The XOR expected is: {}", expected);
 }
 
 fn challenge4() {
-  println!("Challenge 4");
-  // read the file line by line
-  if let Ok(lines) = read_lines("./files/challenge4.txt") {
-    let mut counter = 0;
-    // Consumes the iterator, returns an (Optional) String
-    for line in lines {
-      if let Ok(ip) = line {
-        counter = counter + 1;
-        println!("ciphertext {} --> {}", counter, ip);
-        let resultats = crpt::plausible_text(&ip);
-        for result in resultats.iter() {
-          println!("key is: {}, output is: {}", result.key, result.value);
+    println!("Challenge 4");
+    // read the file line by line
+    if let Ok(lines) = read_lines("./files/challenge4.txt") {
+        let mut counter = 0;
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(ip) = line {
+                counter = counter + 1;
+                println!("ciphertext {} --> {}", counter, ip);
+                let resultats = crpt::plausible_text(&ip);
+                for result in resultats.iter() {
+                    println!("key is: {}, output is: {}", result.key, result.value);
+                }
+            }
         }
-      }
     }
-  }
 }
 
 fn challenge3() {
-  println!("Challenge 3");
-  // The hex encoded string:
-  // 1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736
-  // ... has been XOR'd against a single character. Find the key, decrypt the message.
+    println!("Challenge 3");
+    // The hex encoded string:
+    // 1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736
+    // ... has been XOR'd against a single character. Find the key, decrypt the message.
 
-  let hex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
-  println!("The hex is: {}", hex);
-  println!("Possible plaintext:");
+    let hex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+    println!("The hex is: {}", hex);
+    println!("Possible plaintext:");
 
-  let resultats = crpt::plausible_text(&hex);
-  for result in resultats.iter() {
-    println!("key is: {}, output is: {}", result.key, result.value);
-  }
+    let resultats = crpt::plausible_text(&hex);
+    for result in resultats.iter() {
+        println!("key is: {}, output is: {}", result.key, result.value);
+    }
 
-  println!("Candidates found: {}", resultats.len());
+    println!("Candidates found: {}", resultats.len());
 }
 
 fn challenge2() {
-  println!("Challenge 2");
-  // feed it the string:
-  // 1c0111001f010100061a024b53535009181c
-  // ... after hex decoding, and when XOR'd against:
-  // 686974207468652062756c6c277320657965
-  // ... should produce:
-  // 746865206b696420646f6e277420706c6179
+    println!("Challenge 2");
+    // feed it the string:
+    // 1c0111001f010100061a024b53535009181c
+    // ... after hex decoding, and when XOR'd against:
+    // 686974207468652062756c6c277320657965
+    // ... should produce:
+    // 746865206b696420646f6e277420706c6179
 
-  let hex = "1c0111001f010100061a024b53535009181c";
-  let key = "686974207468652062756c6c277320657965";
-  let expected = "746865206b696420646f6e277420706c6179";
-  let result = crpt::hex_xor(hex, key);
-  assert_eq!(expected, result);
+    let hex = "1c0111001f010100061a024b53535009181c";
+    let key = "686974207468652062756c6c277320657965";
+    let expected = "746865206b696420646f6e277420706c6179";
+    let result = crpt::hex_xor(hex, key);
+    assert_eq!(expected, result);
 
-  println!("The hex is: {}", hex);
-  println!("The key is: {}", key);
-  println!("The XOR output is: {}", result);
-  println!("The XOR output expected is: {}", expected);
+    println!("The hex is: {}", hex);
+    println!("The key is: {}", key);
+    println!("The XOR output is: {}", result);
+    println!("The XOR output expected is: {}", expected);
 }
 
 fn challenge1() {
-  println!("Challenge 1");
-  // The string:
-  // 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
-  // Should produce:
-  // SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+    println!("Challenge 1");
+    // The string:
+    // 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+    // Should produce:
+    // SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
 
-  let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-  let expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
-  let result = crpt::hex_to_base64(hex);
-  assert_eq!(expected, result);
+    let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+    let result = crpt::hex_to_base64(hex);
+    assert_eq!(expected, result);
 
-  println!("The hex value is: {}", hex);
-  println!("The base64 output is: {}", result);
-  println!("The base64 output expected is: {}", expected);
+    println!("The hex value is: {}", hex);
+    println!("The base64 output is: {}", result);
+    println!("The base64 output expected is: {}", expected);
 }
 
 fn wrap(f: &dyn Fn(), run: i8) {
-  if run == 0 {
-    return;
-  }
-  sep();
-  f();
-  sep()
+    if run == 0 {
+        return;
+    }
+    sep();
+    f();
+    sep()
 }
 
 fn sep() {
-  println!("{}", "=====================".to_string());
+    println!("{}", "=====================".to_string());
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
-  P: AsRef<Path>,
+    P: AsRef<Path>,
 {
-  let file = File::open(filename)?;
-  Ok(io::BufReader::new(file).lines())
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
